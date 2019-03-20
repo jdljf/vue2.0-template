ede3d7d755c2958b7e6d781c72840fe15d95b867
jdljf <1243270526@qq.com>
2019-03-20 10:18:06 +0800

    resolve.modules 设置第三方模块寻找路径

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -4,7 +4,7 @@ const utils = require('./utils')
 const config = require('../config')
 const vueLoaderConfig = require('./vue-loader.conf')
 
-function resolve (dir) {
+function resolve(dir) {
   return path.join(__dirname, '..', dir)
 }
 
@@ -36,7 +36,11 @@ module.exports = {
     alias: {
       'vue$': 'vue/dist/vue.esm.js',
       '@': resolve('src'),
-    }
+    },
+    modules: [
+      resolve('src'),
+      resolve('node_modules')
+    ]
   },
   module: {
     rules: [
